4c628ab13f77aa614f9b706e7e60d4087d59433d
rkothari <ronak.kothari@sprinklr.com>
2017-01-13 13:26:43 +0530

    some syntax changes.

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import React, { Component, PropTypes } from 'react';
 import {
-  AppRegistry,
   StyleSheet,
   Text,
   View,
@@ -12,20 +11,20 @@ import {
 
 import { AnimatedCircularProgress } from 'react-native-circular-progress';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import styles from './main.style';
 
 const { width, height } = Dimensions.get( 'window' );
 
-const BUTTON_HEIGHT = 54;
 const AnimatingCicleBackgroundColor = "#bbbbbb";
 console.disableYellowBox = true;
 
 class SubmitButton extends Component {
 
-  constructor() {
-    super();
+  constructor(props, context) {
+    super(props, context);
     this.state = {
       isLoading            : false,
-      animatedWidth        : new Animated.Value( 0 ),
+      animatedValue        : new Animated.Value( 0 ),
       fill                 : 0,
       canShowAnimatedCircle: false,
       isReady              : false
@@ -51,74 +50,78 @@ class SubmitButton extends Component {
   };
 
   componentWillReceiveProps( nextProps ) {
-    if ( nextProps.buttonState === 'success' || nextProps.buttonState === 'error' ) {
-      this.setState( { isLoading: false, isReady: true, canShowAnimatedCircle: false }, this.animateWidthBackToOriginal )
+  const buttonState = nextProps.buttonState;
+    if (buttonState === 'success' || buttonState === 'error' ) {
+      this.setState( { isLoading: false, isReady: true, canShowAnimatedCircle: false }, this.animateBackToOriginal )
     }
   }
 
   render() {
-    const { width, height, primaryColor, secondaryColor, buttonStyle, errorColor } = this.props;
-    const buttonWidth = this.state.animatedWidth.interpolate( {
+    const { width, height, primaryColor, secondaryColor, buttonStyle, errorColor, buttonState } = this.props;
+    const { animatedValue, isLoading, isReady, canShowAnimatedCircle, fill } = this.state;
+    const buttonWidth = animatedValue.interpolate( {
       inputRange : [ 0, 1, 2 ],
       outputRange: [ width, height, width ]
     } );
-    const buttonHeight = height || BUTTON_HEIGHT;
-    const readyStateBorderColor = this.props.buttonState === 'success' ? primaryColor : this.props.buttonState === 'error' ? errorColor : primaryColor;
+    const buttonHeight = height;
+    const readyStateBorderColor = buttonState === 'success' ? primaryColor : buttonState === 'error' ? errorColor : primaryColor;
     let borderColor = primaryColor;
-    borderColor = this.state.isLoading ? AnimatingCicleBackgroundColor : readyStateBorderColor;
-    borderColor = borderColor || primaryColor;
-    const borderWidth = this.state.isLoading ? 4 : 4;
-    const readyStateBgColor = this.props.buttonState === 'success' ? primaryColor : errorColor;
-    const backgroundColor = this.state.isLoading ? 'transparent' : !this.state.isReady ? secondaryColor : readyStateBgColor;
-    const buttonOpacity = this.state.canShowAnimatedCircle ? 0 : 1;
+    borderColor = isLoading ? AnimatingCicleBackgroundColor : readyStateBorderColor;
+    const readyStateBgColor = buttonState === 'success' ? primaryColor : errorColor;
+    const backgroundColor = isLoading ? 'transparent' : !isReady ? secondaryColor : readyStateBgColor;
+    const buttonOpacity = canShowAnimatedCircle ? 0 : 1;
+
     return (
       <Animated.View style={[styles.buttonContainer, {width: buttonWidth }]}>
         <TouchableOpacity
-          style={[styles.button, buttonStyle, {height: buttonHeight ,borderWidth: borderWidth, borderColor: borderColor, backgroundColor: backgroundColor, opacity: buttonOpacity}]}
-          onPress={this.onPressSubmitButton}>
+          style={[styles.button, buttonStyle, {height: buttonHeight , borderColor: borderColor, backgroundColor: backgroundColor, opacity: buttonOpacity}]}
+          onPress={this.onSubmitButtonClick}>
           {this.renderBody()}
         </TouchableOpacity>
-        {this.renderAnimatedCircle()}
+        {this.renderAnimatedCircle(animatedValue, fill, height, primaryColor)}
       </Animated.View>
     );
   }
 
   renderBody = () => {
-    const { buttonTextWhenReady } = this.props;
-    const iconName = this.props.buttonState === 'success' ? this.props.iconName : 'times';
-    const textColor = this.state.isReady ? this.props.secondaryColor : this.props.primaryColor;
-    const textOpacity = this.state.animatedWidth.interpolate( {
+    const { buttonTextWhenReady, iconName, textStyle, buttonText, secondaryColor, buttonState, primaryColor } = this.props;
+    const { animatedValue, isReady } = this.state;
+    const successIconName = buttonState === 'success' ? iconName : 'times';
+    const textColor = isReady ? secondaryColor : primaryColor;
+    const textOpacity = animatedValue.interpolate( {
       inputRange : [ 0, 0.5, 1, 1.5, 2 ],
       outputRange: [ 1, 0, 0, 0, 1 ],
     } );
-    const { textStyle } = this.props;
-    if ( !this.state.isReady ) {
+
+    if ( !isReady ) {
       return (
-        <Animated.Text style={[styles.text, textStyle, {opacity: textOpacity, color: textColor}]}>{this.props.buttonText}</Animated.Text>
+        <Animated.Text style={[styles.text, textStyle, {opacity: textOpacity, color: textColor}]}>{buttonText}</Animated.Text>
       );
-    } else if ( buttonTextWhenReady ) {
+    }
+    if ( buttonTextWhenReady ) {
       return (
         <Animated.Text style={[styles.text, textStyle, {opacity: textOpacity, color: textColor}]}>{buttonTextWhenReady}</Animated.Text>
       );
     }
     return (
-      <Icon name={iconName} size={26} color={this.props.secondaryColor}/>
+      <Icon name={successIconName} size={26} color={secondaryColor}/>
     );
   };
 
-  renderAnimatedCircle = () => {
-    const opacity = this.state.animatedWidth.interpolate( {
+  renderAnimatedCircle = (animatedValue, fill, height, primaryColor) => {
+    const opacity = animatedValue.interpolate( {
       inputRange : [ 0, 0.99, 1, 1.1, 2 ],
       outputRange: [ 0, 0, 1, 0, 0 ],
       extrapolate: 'clamp'
     } );
+
     return (
       <AnimatedCircularProgress
         ref='circularProgress'
-        size={this.props.height}
+        size={height}
         width={4}
-        fill={this.state.fill}
-        tintColor={this.props.primaryColor}
+        fill={fill}
+        tintColor={primaryColor}
         backgroundColor={AnimatingCicleBackgroundColor}
         rotation={0}
         tension={20}
@@ -127,24 +130,30 @@ class SubmitButton extends Component {
     );
   };
 
-  onPressSubmitButton = () => {
-    if ( this.state.isLoading || this.state.isReady ) return;
-    this.props.onSubmit();
+  onSubmitButtonClick = () => {
+    const { isLoading, isReady, animatedValue } = this.state;
+    const { onSubmit, animationDuration } = this.props;
+
+    if ( isLoading || isReady ) return;
+    onSubmit();
     this.setState( { isLoading: true }, () => setTimeout( () => {
       this.setState( { fill: 100, canShowAnimatedCircle: true } )
-    }, this.props.animationDuration ) );
-    Animated.timing( this.state.animatedWidth, {
+    }, animationDuration ) );
+    Animated.timing( animatedValue, {
       toValue : 1,
-      duration: this.props.animationDuration,
+      duration: animationDuration,
       easing  : Easing.linear
     } ).start();
   };
 
-  animateWidthBackToOriginal = () => {
-    const cb = this.props.buttonState === 'success' ? this.props.onSuccess : this.props.onError;
-    Animated.timing( this.state.animatedWidth, {
+  animateBackToOriginal = () => {
+    const { animatedValue } = this.state;
+    const { buttonState, onSuccess, onError, animationDuration } = this.props;
+    const cb = buttonState === 'success' ? onSuccess : onError;
+
+    Animated.timing( animatedValue, {
       toValue : 2,
-      duration: this.props.animationDuration,
+      duration: animationDuration,
       easing  : Easing.linear
     } ).start( cb() );
   }
@@ -171,25 +180,5 @@ SubmitButton.propTypes = {
   onSuccess  : PropTypes.func, // on success callback
   onError    : PropTypes.func // on error callback
 };
-
-const styles = StyleSheet.create( {
-  buttonContainer: {
-    justifyContent: 'center'
-  },
-  button         : {
-    height         : 54,
-    borderRadius   : 35,
-    alignItems     : 'center',
-    justifyContent : 'center',
-    backgroundColor: 'white'
-  },
-  text           : {
-    fontSize       : 22,
-    fontFamily     : 'proximanova-regular',
-    color          : 'rgb(30, 205, 151)',
-    fontWeight     : 'bold',
-    backgroundColor: 'transparent'
-  }
-} );
 
 module.exports = SubmitButton;
---
+++ main.style.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  buttonContainer: {
+    justifyContent: 'center'
+  },
+  button         : {
+    height         : 54,
+    borderRadius   : 35,
+    alignItems     : 'center',
+    justifyContent : 'center',
+    backgroundColor: 'white',
+    borderWidth: 4
+  },
+  text           : {
+    fontSize       : 22,
+    fontFamily     : 'proximanova-regular',
+    color          : 'rgb(30, 205, 151)',
+    fontWeight     : 'bold',
+    backgroundColor: 'transparent'
+  }
+});
